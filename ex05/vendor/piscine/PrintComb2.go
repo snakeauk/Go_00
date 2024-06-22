7ec2644ff0f2fa7888226d6bac3dde04e637c520
kinamura <i.kosuke0801@gmail.com>
2024-06-22 18:57:22 +0900

    create ex05

---
+++ ex05/vendor/piscine/PrintComb2.go
@@ -0,0 +1,29 @@
+package piscine
+
+import "ft"
+
+func PrintInt(nb int) {
+	base := 10
+	if nb >= base {
+		PrintInt(nb / base)
+	}
+	ft.PrintRune('0' + rune(nb%10))
+}
+
+func PrintComb2() {
+	nb2_max := 99
+	nb1_max := nb2_max - 1
+	for nb1 := 0; nb1 <= nb1_max; nb1++ {
+		for nb2 := 0; nb2 <= nb2_max; nb2++ {
+			if nb1 != nb2 {
+				PrintInt(nb1)
+				ft.PrintRune(' ')
+				PrintInt(nb2)
+				if !(nb1 == nb1_max && nb2 == nb2_max) {
+					ft.PrintRune(',')
+					ft.PrintRune(' ')
+				}
+			}
+		}
+	}
+}
